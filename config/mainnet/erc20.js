07d02e97fb592f5c08abd3a02099a8f8fddb47f5
AtomsAreCool <64755060+AtomsAreCool@users.noreply.github.com>
2020-05-23 15:27:12 +0200

    Update erc20.js

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -1,18 +1,18 @@
 export default {
-  swap: {
+  befx: {
     address: '0xb91c2a2b953d72f3ef890490669a0a41b0add5f7',
     decimals: 8,
-    fullName: 'BEFX',
+    fullName: 'Belifex',
   },
-  snm: {
+  muxe: {
     address: '0x515669d308f887fd83a471c7764f5d084886d34d',
     decimals: 18,
-    fullName: 'MUXE',
+    fullName: 'Muxe',
   },
-  eurs: {
+  idrt: {
     address: '0x998FFE1E43fAcffb941dc337dD0468d52bA5b48A',
     decimals: 2,
-    fullName: 'IDRT',
+    fullName: 'Rupiah Token',
   },
   usdt: {
     address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
